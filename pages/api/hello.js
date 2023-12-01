29d70b89224e1a2b45cc77ee781fe090ecbd4bb4
Showrov Islam <63032286+showrov29@users.noreply.github.com>
2023-12-01 17:54:11 +0600

    cors

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import * as sdk from "microsoft-cognitiveservices-speech-sdk";
 import fs from "fs";
-import Cors from "cors";
 
 export default async function handler(req, res) {
 	// let text = "";
